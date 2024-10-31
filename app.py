fa958b3c7b084780127d0b1f597f8d864aab4ff4
WGBooth <23709589+WGBooth@users.noreply.github.com>
2024-10-31 07:35:34 -0400

    added route to database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from William Booth in 3308!'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://nba_players_user:PB0ky7JPS5pCvFOdThEnJXbN6xHPU3EM@dpg-cshmjce8ii6s73bhp5eg-a/nba_players")
+    conn.close()
+    return "Database Connection Successful"
