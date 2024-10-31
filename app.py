eab7928d0dcc7aaeb5cfe54f36cf6fb4dd0c43ff
WGBooth <23709589+WGBooth@users.noreply.github.com>
2024-10-31 07:44:58 -0400

    added route to create basketball table

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def db_test():
     conn = psycopg2.connect("postgresql://nba_players_user:PB0ky7JPS5pCvFOdThEnJXbN6xHPU3EM@dpg-cshmjce8ii6s73bhp5eg-a/nba_players")
     conn.close()
     return "Database Connection Successful"
+
+@ app.route('/db_create')
+def crate_basketball():
+    conn = psycopg2.connect("postgresql://nba_players_user:PB0ky7JPS5pCvFOdThEnJXbN6xHPU3EM@dpg-cshmjce8ii6s73bhp5eg-a/nba_players")
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball (
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255), 
+                Number int
+                );
+            ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
